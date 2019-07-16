de1e5ac5b0e9c355ca0823d5395dfd474f33ce8b
SXiaoL <46419181+SXiaoL@users.noreply.github.com>
2019-07-16 16:58:41 +0800

    1.1

--- api/src/main/java/com/yootk/dubbo/service/IEmpService.java
+++ api/src/main/java/com/yootk/dubbo/service/IEmpService.java
@@ -11,4 +11,13 @@ public interface IEmpService {
      * @return 登录成功返回Vo类，登录失败，返回NULL
      */
     public Emp get(String phone);
+
+    /**
+     * 更新用户最后一次登录的时间，和最后一次登录的ip地址
+     * @param lastrecord 最后登录时间
+     * @param ip 最后登录的ip
+     * @param phone 用户手机号（ID）
+     * @return 成功返回true，失败返回false
+     */
+    public boolean updateDateAndIp(String lastrecord,String ip,String phone);
 }
