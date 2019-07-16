ed33f4bbd50455802a6f32eb5e555bc032258c1a
SXiaoL <46419181+SXiaoL@users.noreply.github.com>
2019-07-16 21:58:35 +0800

    修改头像

---
+++ web-oa/src/main/java/com/yootk/oa/service/IEmpPrivilegeService.java
@@ -0,0 +1,9 @@
+package com.yootk.oa.service;
+
+
+import com.yootk.dubbo.vo.Emp;
+
+public interface IEmpPrivilegeService {
+    public Emp getByPhone(String phone);
+    public boolean setDateAndIp(String lastrecord, String ip, String phone);
+}
