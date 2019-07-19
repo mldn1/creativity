be5e3552c249267d7f9bdd7c355b947e931ce028
SXiaoL <46419181+SXiaoL@users.noreply.github.com>
2019-07-19 17:06:04 +0800

    完成用户登录日志记录

--- web-mall/src/main/java/com/yootk/mall/action/CommonAction.java
+++ web-mall/src/main/java/com/yootk/mall/action/CommonAction.java
@@ -1,21 +1,28 @@
 package com.yootk.mall.action;
 
+import com.yootk.dubbo.vo.EmpLogs;
+import com.yootk.dubbo.vo.MemberLogs;
+import com.yootk.mall.service.IMemberLogsService;
+import com.yootk.util.action.AbstractAction;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Controller
-public class CommonAction {
+public class CommonAction extends AbstractAction {
+
     @RequestMapping("login")
     public String  login(){
+
         return "front/login/login-2" ;
     }
     @RequestMapping("login_pswd")
     public String  login_pswd(String username,String password){
+
         return "front/login/login-3" ;
-    }
-    @RequestMapping("/pages/welcome")
-    public String welcome() {
-        return "front/main/index";
     }
     @RequestMapping("logoutInfo")
     public String logoutInfo() {
